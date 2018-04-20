d6d179016320481a474a9108ef3c57c94205481f
zhouxiaolong <whoiszxl@gmail.com>
2018-04-21 01:35:32 +0800

    实现动态获取iconslist数据，且分swiper

--- code/project/Travel/src/pages/home/components/Icons.vue
+++ code/project/Travel/src/pages/home/components/Icons.vue
@@ -1,101 +1,117 @@
 <template>
     <div class="icons">
-      <div class="icon">
-        <div class="icon-img">
-          <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="">
-        </div>
-        <p class="icon-desc">景点门票</p>
-      </div>
-
-      <div class="icon">
-        <div class="icon-img">
-          <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="">
-        </div>
-        <p class="icon-desc">景点门票</p>
-      </div>
-
-      <div class="icon">
-        <div class="icon-img">
-          <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="">
-        </div>
-        <p class="icon-desc">景点门票</p>
-      </div>
-
-      <div class="icon">
-        <div class="icon-img">
-          <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="">
-        </div>
-        <p class="icon-desc">景点门票</p>
-      </div>
-
-      <div class="icon">
-        <div class="icon-img">
-          <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="">
-        </div>
-        <p class="icon-desc">景点门票</p>
-      </div>
-
-      <div class="icon">
-        <div class="icon-img">
-          <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="">
-        </div>
-        <p class="icon-desc">景点门票</p>
-      </div>
-
-      <div class="icon">
-        <div class="icon-img">
-          <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="">
-        </div>
-        <p class="icon-desc">景点门票</p>
-      </div>
-
-      <div class="icon">
-        <div class="icon-img">
-          <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" alt="">
-        </div>
-        <p class="icon-desc">景点门票</p>
-      </div>
+      <swiper>
+        <swiper-slide v-for="(page, index) of pages" :key="index">
+          <div class="icon" v-for="item of page" :key="item.id">
+            <div class="icon-img">
+              <img class="icon-img-content" :src="item.imgUrl" alt="">
+            </div>
+            <p class="icon-desc">{{item.desc}}</p>
+          </div>
+        </swiper-slide>
+      </swiper>
     </div>
 </template>
 
 <script>
 export default {
-  name: 'HomeIcons'
+  name: 'HomeIcons',
+  data () {
+    return {
+      iconList: [{
+        id: '001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票'
+      }, {
+        id: '002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/8246f27355943202.png',
+        desc: '游乐场'
+      }, {
+        id: '003',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/bd/9f7b9b2b60c1502.png',
+        desc: '踏青赏花'
+      }, {
+        id: '004',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/b6/37560ece9c62b502.png',
+        desc: '城市观光'
+      }, {
+        id: '005',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+        desc: '一日游'
+      }, {
+        id: '006',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png',
+        desc: '动植物园'
+      }, {
+        id: '007',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/27/dac2bcf9added702.png',
+        desc: '海滨海岛'
+      }, {
+        id: '008',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/b6/37560ece9c62b502.png',
+        desc: '东部华侨城'
+      }, {
+        id: '009',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/50/26ffa31b56646402.png',
+        desc: '海洋馆'
+      }, {
+        id: '010',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/80/416c6ab3368d1f02.png',
+        desc: '全部玩乐'
+      } ]
+    }
+  },
+  // 通过计算属性，来通过除法来给所有的数据分组，小于等于8的一组，大于的一组
+  // 再将每次遍历的实际对象存入到分别的组中，然后实际遍历的时候就用这个pages的二维数组了
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
   @import '~styles/varibles.styl'
-  .icons
-    overflow: hidden
+  @import '~styles/mixins.styl'
+  .icons >>> .swiper-container
     height: 0
     padding-bottom: 50%
-    .icon
-      position relative
-      overflow: hidden
-      float: left
-      width: 25%
-      height 0
-      padding-bottom: 25%
-      .icon-img
-        position absolute
-        top 0
-        left 0
-        right 0
-        bottom .44rem
-        box-sizing border-box
-        padding .1rem
-        .icon-img-content
-          display block
-          margin 0 auto
-          height 100%
-      .icon-desc
-        position absolute
-        left 0
-        right 0
-        bottom 0
-        height .44rem
-        line-height .44rem
-        text-align center
-        color $darkTextColor
+  .icon
+    position relative
+    overflow: hidden
+    float: left
+    width: 25%
+    height 0
+    padding-bottom: 25%
+    .icon-img
+      position absolute
+      top 0
+      left 0
+      right 0
+      bottom .44rem
+      box-sizing border-box
+      padding .1rem
+      .icon-img-content
+        display block
+        margin 0 auto
+        height 100%
+    .icon-desc
+      position absolute
+      left 0
+      right 0
+      bottom 0
+      height .44rem
+      line-height .44rem
+      text-align center
+      color $darkTextColor
+      ellipsis()
 </style>
