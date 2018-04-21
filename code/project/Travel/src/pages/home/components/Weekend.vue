bf5da99bb81567b1e959badbe134115f520b2c79
zhouxiaolong <whoiszxl@gmail.com>
2018-04-21 21:34:36 +0800

    周末去哪儿的组件开发

---
+++ code/project/Travel/src/pages/home/components/Weekend.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <div class="recommend-title">周末去哪儿</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl">
+        </div>
+
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      recommendList: [{
+        id: '001',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/eb/e4b181a6957c36.jpg_r_640x214_4abbdbc0.jpg',
+        title: '情侣好去处',
+        desc: '手牵手，一起走到老'
+      }, {
+        id: '002',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/fa/ca65fde9677de2.jpg_r_640x214_4500e3ff.jpg',
+        title: '深圳必游TOP10',
+        desc: '来到深圳，你需要来这里签到'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .recommend-title
+    margin-top .2rem
+    line-height .8rem
+    background #ccc
+    text-indent .2rem
+  .item-img-wrapper
+    overflow hidden
+    height 0
+    padding-bottom 33.9%
+    .item-img
+      width 100%
+  .item-info
+    padding .1rem
+    .item-title
+      line-height .54rem
+      font-size .32rem
+      ellipsis()
+    .item-desc
+      line-height .4rem
+      color #ccc
+      ellipsis()
+
+</style>
