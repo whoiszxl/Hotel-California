578f0aae8c2ff5cb12971c92c9734089ea9da3b7
zhouxiaolong <whoiszxl@gmail.com>
2018-04-22 12:20:43 +0800

    city路由和header

---
+++ code/project/Travel/src/pages/city/City.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <city-header></city-header>
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios'
+import CityHeader from './components/Header'
+export default {
+  name: 'City',
+  components: {
+    CityHeader
+  },
+  data () {
+    return {
+      city: '',
+      swiperList: [],
+      iconList: [],
+      recommendList: [],
+      weekendList: []
+    }
+  },
+  methods: {
+    getHomeInfo () {
+      axios.get('/api/index.json')
+        .then(this.getHomeInfoSucc)
+    },
+    getHomeInfoSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.city = data.city
+        this.swiperList = data.swiperList
+        this.iconList = data.iconList
+        this.recommendList = data.recommendList
+        this.weekendList = data.weekendList
+      }
+      console.log(res)
+    }
+  },
+  mounted () {
+    this.getHomeInfo()
+  }
+}
+</script>
+
+<style>
+
+</style>
--- code/project/Travel/src/pages/home/components/Header.vue
+++ code/project/Travel/src/pages/home/components/Header.vue
@@ -4,7 +4,9 @@
       <div class="iconfont back-icon">&#xe624;</div>
     </div>
     <div class="header-input"><span class="iconfont">&#xe632;</span>输入城市/景点/游玩主题</div>
-    <div class="header-right">{{this.city}}<span class="iconfont arrow-icon">&#xe64a;</span></div>
+    <router-link to="/city">
+      <div class="header-right">{{this.city}}<span class="iconfont arrow-icon">&#xe64a;</span></div>
+    </router-link>
   </div>
 </template>
 
