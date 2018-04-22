9b1599006043c783b942a41736324990b1614fe8
zhouxiaolong <whoiszxl@gmail.com>
2018-04-22 21:44:10 +0800

    城市列表：字母表点击和滑动切换城市列表

--- code/project/Travel/src/pages/city/City.vue
+++ code/project/Travel/src/pages/city/City.vue
@@ -2,8 +2,12 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
-    <city-list :cities="cities" :hot="hotCities"></city-list>
-    <city-alphabet :cities="cities"></city-alphabet>
+    <city-list
+      :cities="cities"
+      :hot="hotCities"
+      :letter="letter"
+      ></city-list>
+    <city-alphabet :cities="cities" @change="handleLetterChange"></city-alphabet>
   </div>
 </template>
 
@@ -25,7 +29,8 @@ export default {
   data () {
     return {
       cities: {},
-      hotCities: []
+      hotCities: [],
+      letter: ''
     }
   },
   methods: {
@@ -41,6 +46,9 @@ export default {
         this.hotCities = data.hotCities
       }
       console.log(res)
+    },
+    handleLetterChange (letter) {
+      this.letter = letter
     }
   },
   mounted () {
