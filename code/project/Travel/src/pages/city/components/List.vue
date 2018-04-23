244508106d749e8fb07fabb044ecd7d29f21827b
zhouxiaolong <whoiszxl@gmail.com>
2018-04-23 11:43:35 +0800

    使用vuex实现城市选择页面和主页面数据共享

--- code/project/Travel/src/pages/city/components/List.vue
+++ code/project/Travel/src/pages/city/components/List.vue
@@ -6,7 +6,7 @@
         <div class="title border-topbottom">当前城市</div>
         <div class="button-list">
           <div class="button-wrapper">
-            <div class="button">深圳</div>
+            <div class="button">{{this.$store.state.city}}</div>
           </div>
         </div>
       </div>
@@ -14,7 +14,7 @@
       <div class="area">
         <div class="title border-topbottom">热门城市</div>
         <div class="button-list">
-          <div class="button-wrapper" v-for="item of hot" :key="item.id">
+          <div class="button-wrapper" v-for="item of hot" :key="item.id" @click="handleCityClick(item.name)">
             <div class="button">{{item.name}}</div>
           </div>
         </div>
@@ -23,7 +23,7 @@
       <div class="area" v-for="(item, key) of cities" :key="key" :ref="key">
         <div class="title border-topbottom">{{key}}</div>
         <div class="item-list">
-          <div class="item border-bottom" v-for="citem of item" :key="citem.id">{{citem.name}}</div>
+          <div class="item border-bottom" v-for="citem of item" :key="citem.id" @click="handleCityClick(citem.name)">{{citem.name}}</div>
         </div>
       </div>
 
@@ -40,8 +40,11 @@ export default {
     hot: Array,
     letter: String
   },
-  mounted () {
-    this.scroll = new Bscroll(this.$refs.wrapper)
+  methods: {
+    handleCityClick (city) {
+      this.$store.dispatch('changeCity', city)
+      this.$router.push('/')
+    }
   },
   watch: {
     letter () {
@@ -50,6 +53,9 @@ export default {
         this.scroll.scrollToElement(element)
       }
     }
+  },
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.wrapper)
   }
 }
 </script>
