65ac3cfdcc488f162731f9f40fc6f6e7edf07403
zhouxiaolong <whoiszxl@gmail.com>
2018-04-25 00:04:00 +0800

    公共组件封装：画廊

--- code/project/Travel/src/pages/detail/components/Banner.vue
+++ code/project/Travel/src/pages/detail/components/Banner.vue
@@ -1,16 +1,38 @@
 <template>
-  <div class="banner">
-    <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1412/df/94274dc87270c48567ff1f8c2be2bc94.water.jpg_600x330_9717ac06.jpg" alt="">
-    <div class="banner-info">
-      <div class="banner-title">欢乐海岸</div>
-      <div class="banner-number"><span class="iconfont banner-icon">&#xe692;</span> 100</div>
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1412/df/94274dc87270c48567ff1f8c2be2bc94.water.jpg_600x330_9717ac06.jpg" alt="">
+      <div class="banner-info">
+        <div class="banner-title">欢乐海岸</div>
+        <div class="banner-number"><span class="iconfont banner-icon">&#xe692;</span> 100</div>
+      </div>
     </div>
+    <common-gallary :imgs="imgs" v-show="showGallary" @close="handleCloseClick"></common-gallary>
   </div>
 </template>
 
 <script>
+import CommonGallary from 'common/gallary/Gallary'
 export default {
-  name: 'DetailBanner'
+  name: 'DetailBanner',
+  methods: {
+    handleBannerClick () {
+      this.showGallary = true
+    },
+
+    handleCloseClick () {
+      this.showGallary = false
+    }
+  },
+  data () {
+    return {
+      showGallary: false,
+      imgs: ['http://img1.qunarzz.com/sight/p0/1507/ac/5ca7438b49fe4a1ea906ed2c4238b076.water.jpg_r_800x800_787a1885.jpg', 'http://img1.qunarzz.com/sight/p0/1507/f1/89f9c504930f86f86b877ed28b98fa2c.water.jpg_r_800x800_6c21161d.jpg']
+    }
+  },
+  components: {
+    CommonGallary
+  }
 }
 </script>
 
