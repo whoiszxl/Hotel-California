cae220bb290f867e6e1d62eca0fc040f97d2d6d5
zhouxiaolong <whoiszxl@gmail.com>
2018-04-24 23:23:59 +0800

    详情页：banner

---
+++ code/project/Travel/src/pages/detail/components/Banner.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="banner">
+    <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1412/df/94274dc87270c48567ff1f8c2be2bc94.water.jpg_600x330_9717ac06.jpg" alt="">
+    <div class="banner-info">
+      <div class="banner-title">欢乐海岸</div>
+      <div class="banner-number"><span class="iconfont banner-icon">&#xe692;</span> 100</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailBanner'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    position relative
+    overflow hidden
+    height 0
+    padding-bottom 55%
+    .banner-img
+      width 100%
+    .banner-info
+      color #fff
+      display flex
+      position absolute
+      left 0
+      right 0
+      bottom 0
+      line-height .6rem
+      background-image linear-gradient(top, rgba(0,0,0,0), rgba(0,0,0,0.8))
+      .banner-title
+        flex 1
+        font-size .32rem
+        padding 0 .2rem
+      .banner-number
+        height .32rem
+        line-height .32rem
+        padding 0 .4rem
+        margin-top .14rem
+        height .4rem
+        border-radius .2rem
+        background rgba(0,0,0,.8)
+        font-size .24rem
+        .banner-icon
+          font-size .24rem
+</style>
