45c5c5505d2b2a7d1d02d9ab2168374f4cae99ba
zhouxiaolong <whoiszxl@gmail.com>
2018-04-25 18:49:52 +0800

    header渐隐效果

--- code/project/Travel/src/pages/detail/components/Header.vue
+++ code/project/Travel/src/pages/detail/components/Header.vue
@@ -1,19 +1,49 @@
 <template>
   <div>
-    <router-link tag="div" to="/" class="header-abs">
+    <router-link tag="div" to="/" class="header-abs" v-show="showAbs">
       <div class="iconfont header-abs-back">&#xe624;</div>
     </router-link>
-    <div class="header-fixed"></div>
+    <div class="header-fixed" v-show="!showAbs" :style="opacityStyle">
+      <router-link to="/"><div class="iconfont header-fixed-back">&#xe624;</div></router-link>
+      景点详情
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'DetailHeader'
+  name: 'DetailHeader',
+  data () {
+    return {
+      showAbs: true,
+      opacityStyle: {
+        opacity: 0
+      }
+    }
+  },
+  methods: {
+    handleScroll () {
+      const top = document.documentElement.scrollTop
+      if (top > 40) {
+        let opacity = top / 140
+        opacity = opacity > 1 ? 1 : opacity
+        this.opacityStyle = {
+          opacity
+        }
+        this.showAbs = false
+      } else {
+        this.showAbs = true
+      }
+    }
+  },
+  activated () {
+    window.addEventListener('scroll', this.handleScroll)
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
   .header-abs
     position absolute
     left .2rem
@@ -27,4 +57,23 @@ export default {
     .header-abs-back
       color #fff
       font-size .4rem
+  .header-fixed
+    position fixed
+    top 0
+    left 0
+    right 0
+    height $headerHeight
+    line-height $headerHeight
+    text-align center
+    color #fff
+    background $bgColor
+    font-size .35rem
+    .header-fixed-back
+      position absolute
+      top 0
+      left 0
+      width .64rem
+      text-align center
+      font-size .4rem
+      color #fff
 </style>
